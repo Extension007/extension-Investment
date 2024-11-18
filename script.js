4c802ca0c4153a0f88d6e4f80feb60c6d304c52c
Extension007 <investmentextension@gmail.com>
2024-11-18 14:31:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,33 @@ const translations = {
         "section2-title": "Токеномика",
         "section2-text": "Токеномика",
         "section3-title": "Дорожная карта",
-        "section3-text": "Дорожная карта",
+        "section3-text": [
+            "Формирование идеи проекта — Определение основной концепции и целей.",
+            "Разработка веб-приложения — Создание первого прототипа и базового функционала.",
+            "Выпуск токена — Запуск собственной криптовалюты для экосистемы.",
+            "Создание социальных сетей — Открытие официальных аккаунтов для взаимодействия с аудиторией.",
+            "Pre-sale — Продажа токенов на ранней стадии для привлечения инвестиций.",
+            "Запуск пула — Создание пула ликвидности для обмена и хранения токенов.",
+            "Запуск приложения для набора поинтов — Внедрение системы начисления поинтов для сжигания токенов.",
+            "Запуск LaunchPad — предоставление инвесторам войти в проект на ранней стадии.",
+            "Масштабирование проекта — Расширение инфраструктуры и повышение нагрузки.",
+            "Запуск первого раунда сжигания — Начало процесса сжигания токенов для контроля их эмиссии.",
+            "Выпуск коллекции NFT — Запуск уникальных коллекционных токенов для пользователей.",
+            "Запуск второго раунда сжигания — Второй этап сжигания токенов для поддержания их ценности.",
+            "Расширение партнерств и коллабораций — Привлечение новых партнеров и расширение сети.",
+            "Запуск третьего раунда сжигания — Финальный этап сжигания токенов.",
+            "Релиз новых функций в приложении — Добавление новых возможностей и улучшений для пользователей.",
+            "Запуск маркетинговой кампании — Старт рекламных кампаний для привлечения внимания.",
+            "Выход на новые рынки и расширение аудитории — Привлечение пользователей из других регионов.",
+            "Оптимизация экосистемы — Улучшение всех компонентов проекта для повышения производительности.",
+            "Запуск блокчейна — Запуск собственной блокчейн-сети для управления проектом.",
+            "Запуск инвестиционного фонда — Создание инвестиционного фонда для поддержки развития.",
+            "Продолжение инноваций и внедрение новых технологий — Постоянное обновление и внедрение новых технологий."
+        ],
         "section4-title": "Белый лист",
         "section4-text": "Белый лист",
-        "section5-title": "Колаборации",
-        "section5-text": "Колаборации",
+        "section5-title": "Коллаборации",
+        "section5-text": "Коллаборации",
         "section6-title": "Реклама",
         "section6-text": "Реклама",
         "footer": "© 2024 EXTO Ecosystem"
@@ -22,11 +44,33 @@ const translations = {
         "section2-title": "Tokenomics",
         "section2-text": "Tokenomics",
         "section3-title": "Roadmap",
-        "section3-text": "Roadmap",
+        "section3-text": [
+            "Project Idea Formation - Define the main concept and goals.",
+            "Web Application Development - Create the first prototype and basic functionality.",
+            "Token Launch - Launch own cryptocurrency for the ecosystem.",
+            "Social Media Creation - Open official accounts for audience engagement.",
+            "Pre-sale - Token sale in early stages to raise investment.",
+            "Pool Launch - Create a liquidity pool for token exchange and storage.",
+            "Point System Launch - Implement a point system for token burning.",
+            "LaunchPad Launch - Provide early-stage investment opportunities.",
+            "Project Scaling - Expand infrastructure and increase load capacity.",
+            "First Burn Round Launch - Start the token burn process to control emission.",
+            "NFT Collection Launch - Release unique collectible tokens for users.",
+            "Second Burn Round Launch - Second phase of token burning to support value.",
+            "Partnership Expansion - Attract new partners and expand the network.",
+            "Third Burn Round Launch - Final phase of token burning.",
+            "New Features Release - Add new functionalities and improvements for users.",
+            "Marketing Campaign Launch - Start advertising campaigns to attract attention.",
+            "Market Expansion - Attract users from other regions.",
+            "System Optimization - Improve ecosystem components for higher performance.",
+            "Blockchain Launch - Launch own blockchain network for project management.",
+            "Investment Fund Launch - Create an investment fund to support development.",
+            "Ongoing Innovation - Constantly update and integrate new technologies."
+        ],
         "section4-title": "White Paper",
         "section4-text": "White Paper",
-        "section5-title": "Collaboration",
-        "section5-text": "Collaboration",
+        "section5-title": "Collaborations",
+        "section5-text": "Collaborations",
         "section6-title": "Advertising",
         "section6-text": "Advertising",
         "footer": "© 2024 EXTO Ecosystem"
@@ -38,7 +82,29 @@ const translations = {
         "section2-title": "代币经济学",
         "section2-text": "代币经济学",
         "section3-title": "路线图",
-        "section3-text": "路线图",
+        "section3-text": [
+            "项目创意形成 — 定义主要概念和目标。",
+            "Web应用开发 — 创建第一个原型和基本功能。",
+            "代币发行 — 为生态系统推出自有加密货币。",
+            "社交媒体创建 — 打开官方账户与受众互动。",
+            "Pre-sale — 在早期阶段进行代币销售以筹集投资。",
+            "池子启动 — 创建流动性池用于代币交换和存储。",
+            "积分系统启动 — 实施积分系统用于代币销毁。",
+            "LaunchPad启动 — 为投资者提供项目早期阶段的参与机会。",
+            "项目扩展 — 扩展基础设施并增加负载能力。",
+            "首次销毁轮启动 — 开始代币销毁过程以控制发行量。",
+            "NFT系列发布 — 发布独特的收藏代币给用户。",
+            "第二次销毁轮启动 — 第二阶段销毁代币以维持其价值。",
+            "合作伙伴扩展 — 吸引新合作伙伴并扩大网络。",
+            "第三次销毁轮启动 — 代币销毁的最终阶段。",
+            "新功能发布 — 添加新的功能和改进以提升用户体验。",
+            "营销活动启动 — 启动广告活动以吸引关注。",
+            "市场扩展 — 吸引其他地区的用户。",
+            "系统优化 — 提高项目各个组件的性能。",
+            "区块链启动 — 启动自有区块链网络以管理项目。",
+            "投资基金启动 — 创建投资基金支持发展。",
+            "持续创新 — 不断更新和整合新技术。"
+        ],
         "section4-title": "白皮书",
         "section4-text": "白皮书",
         "section5-title": "合作",
@@ -61,7 +127,7 @@ function changeLanguage() {
     document.documentElement.lang = lang;
 
     try {
-        // Обновление текстов секций
+        // Обновление текста на странице
         document.getElementById('title').innerText = translations[lang].title;
         document.getElementById('section1-title').innerText = translations[lang]['section1-title'];
         document.getElementById('section2-title').innerText = translations[lang]['section2-title'];
@@ -70,7 +136,6 @@ function changeLanguage() {
         document.getElementById('section5-title').innerText = translations[lang]['section5-title'];
         document.getElementById('section6-title').innerText = translations[lang]['section6-title'];
 
-        // Обновление заголовков секций (section-header)
         document.getElementById('section1-header').innerText = translations[lang]['section1-title'];
         document.getElementById('section2-header').innerText = translations[lang]['section2-title'];
         document.getElementById('section3-header').innerText = translations[lang]['section3-title'];
@@ -78,10 +143,14 @@ function changeLanguage() {
         document.getElementById('section5-header').innerText = translations[lang]['section5-title'];
         document.getElementById('section6-header').innerText = translations[lang]['section6-title'];
 
-        // Обновление текста секций (section-text)
+        // Обновление текста секций
         document.getElementById('section1-text').innerText = translations[lang]['section1-text'];
         document.getElementById('section2-text').innerText = translations[lang]['section2-text'];
-        document.getElementById('section3-text').innerText = translations[lang]['section3-text'];
+        
+        // Обновление списка дорожной карты
+        const roadmap = document.getElementById('roadmap');
+        roadmap.innerHTML = translations[lang]['section3-text'].map(item => `<li>${item}</li>`).join('');
+
         document.getElementById('section4-text').innerText = translations[lang]['section4-text'];
         document.getElementById('section5-text').innerText = translations[lang]['section5-text'];
         document.getElementById('section6-text').innerText = translations[lang]['section6-text'];
